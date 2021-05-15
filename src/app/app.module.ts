5507d6ece179a7c58e5e96d18cef1f8fc4457e8e
sngina <sngina707@gmail.com>
2021-05-15 12:36:22 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {quotes }from '../quotes';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+  quotes:quotes[]=[
+    new quotes(1, 'Don’t Let Yesterday Take Up Too Much Of Today.' , 'Will Rogers',new Date(2020,1,1)),
+    new quotes(2, 'The Man Who Has Confidence In Himself Gains The Confidence Of Others.','Hasidic Proverb.' ,new Date(2020,2,12)),
+    new quotes(3, 'A Room Without Books Is Like A Body Without A Soul.', 'Marcus Tullius Cicero.',new Date(2020,3,17)),
+    new quotes(4, 'We May Encounter Many Defeats But We Must Not Be Defeated.' , 'Maya Angelou',new Date(2020,5,21)),
+    new quotes(5, 'Whether You Think You Can Or Think You Can’t, You’re Right.' , 'Henry Ford',new Date(2020,6,12)),
+    new quotes(6, 'Do What You Can With All You Have, Wherever You Are' , 'Theodore Roosevelet',new Date(2020,7,5)),
+    new quotes(7, 'Today’s Accomplishments Were Yesterday’s Impossibilities.' , 'Robert H.Schuller',new Date(2020,8,13)),
+ 
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
