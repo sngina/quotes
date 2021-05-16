cc004082c79361194bd86db856d4da38776c4625
sngina <sngina707@gmail.com>
2021-05-16 13:26:40 +0300

    change form

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,5 +1,5 @@
 export class Quotes {
-constructor(public id:number,public quote:string,  public author:string, public name:string, public completeDate:Date ){
+constructor(public id:number,public quote:string,  public author:string, public name:string, public date:Date ){
 
 } 
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,11 +9,11 @@ import {Quotes }from '../quotes';
 export class QuotesComponent implements OnInit {
   quotes:Quotes[]=[
     new Quotes(1, 'Don’t Let Yesterday Take Up Too Much Of Today.' , 'Will Rogers', 'Stella Ngina',new Date(2020,1,1)),
-    new Quotes(2, 'The Man Who Has Confidence In Himself Gains The Confidence Of Others.','Hasidic Proverb.', 'Stella Ngina' ,new Date(2020,2,12)),
-    new Quotes(3, 'A Room Without Books Is Like A Body Without A Soul.', 'Marcus Tullius Cicero.', 'Stella Ngina',new Date(2020,3,17)),
-    new Quotes(4, 'We May Encounter Many Defeats But We Must Not Be Defeated.' , 'Maya Angelou', 'Stella Ngina' ,new Date(2020,5,21)),
-    new Quotes(5, 'Whether You Think You Can Or Think You Can’t, You’re Right.' , 'Henry Ford', 'Stella Ngina',new Date(2020,6,12)),
-    new Quotes(6, 'Do What You Can With All You Have, Wherever You Are' , 'Theodore Roosevelet','Stella Ngina',new Date(2020,7,5)),
+    new Quotes(2, 'The Man Who Has Confidence In Himself Gains The Confidence Of Others.','Hasidic Proverb.',   'Stella Ngina' ,new Date(2020,2,12)),
+    new Quotes(3, 'A Room Without Books Is Like A Body Without A Soul.', 'Marcus Tullius Cicero.',   'Stella Ngina',new Date(2020,3,17)),
+    new Quotes(4, 'We May Encounter Many Defeats But We Must Not Be Defeated.' , 'Maya Angelou',   'Stella Ngina' ,new Date(2020,5,21)),
+    new Quotes(5, 'Whether You Think You Can Or Think You Can’t, You’re Right.' , 'Henry Ford',   'Stella Ngina',new Date(2020,6,12)),
+    new Quotes(6, 'Do What You Can With All You Have, Wherever You Are' , 'Theodore Roosevelet',  'Stella Ngina',new Date(2020,7,5)),
     new Quotes(7, 'Today’s Accomplishments Were Yesterday’s Impossibilities.' , 'Robert H.Schuller', 'Stella Ngina',new Date(2020,8,13)),
  
   ];
