bf2677395a3d2e2725d65c06122ed3f0fd323eb5
sngina <sngina707@gmail.com>
2021-05-16 13:37:18 +0300

    add form input

--- src/app/quotes.spec.ts
+++ src/app/quotes.spec.ts
@@ -2,6 +2,6 @@ import { Quotes } from './quotes';
 
 describe('Quotes', () => {
   it('should create an instance', () => {
-    expect(new Quote()).toBeTruthy();
+    expect(new Quotes()).toBeTruthy();
   });
 });
